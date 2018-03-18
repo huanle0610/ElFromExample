b94f59552fa2e9df627ea6a1329f3db2a45bd2a3
huanle0610 <735064191@qq.com>
2018-03-18 22:09:19 +0800

    tabs hard

---
+++ src/plan.js
@@ -0,0 +1,13 @@
+function planFormData () {
+  return {
+    name: "",
+    region: "",
+    date1: "",
+    date2: "",
+    delivery: false,
+    type: [],
+    resource: "",
+    desc: ""
+  }
+}
+export default planFormData
